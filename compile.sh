b2df18b68b0ef89549d77ee78d43a4c8d12913cd
AngusP <a7cp3ar50n@gmail.com>
2015-06-03 18:40:45 +0000

    Renamed serd source file

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Compiling serd..."
-gcc serial2zmqsockd.c -o serd -lczmq -lzmq
+gcc serd.c -o serd -lczmq -lzmq
 echo "Compiling test..."
 gcc czmq_subscriber.c -o test -lczmq -lzmq
 echo "Done."
