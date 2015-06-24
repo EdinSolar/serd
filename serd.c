000ff2fe89eb6dfe99e1b6c24de300658bf4d63c
AngusP <a7cp3ar50n@gmail.com>
2015-06-24 20:16:02 +0000

    Added command line arguments -p/-port and -s-serialport to change publisher unix port and tty to use. Still has sensible defaults for RasPi

--- serd.c
+++ serd.c
@@ -8,15 +8,16 @@
 #include <signal.h>     /* Signal handling (e.g. SIGTERM, SIGINT) */
 #include <unistd.h>     /* (UART) Contains POSIX symbolic constants */
 #include <syslog.h>     /* System log library */
-#include <termios.h>    /* (UART) Serial line control */
+#include <termios.h>    /* (UART) Serial line config & control */
 #include <czmq.h>       /* C ZeroMQ Binding */
 
 /* Default TTY */
 #define DEFAULT_TTY "/dev/ttyAMA0"
 
-#define SERD_SOCKET "tcp://127.0.0.1:"
+#define SERD_SOCKET ">tcp://127.0.0.1:"
 /* Defauly publish port */
-#define SERD_DEF_PRT 5000
+#define SERD_DEF_PRT "5000"
+
 #define SER_READ_TIMEOUT 20
 /* Max size of input serial buffer, in chars */
 #define SERIAL_BUFFER_SIZE 4000
@@ -36,6 +37,10 @@ unsigned char serialbuff[SERIAL_BUFFER_SIZE+1];
 /* Serial port config struct */
 struct termios io;
 
+/* Strings to hold config from cli */
+char pub_port[6] = SERD_DEF_PRT;
+char use_tty[24] = DEFAULT_TTY;
+
 /* Function prototypes: */
 void rundaemon(void);
 void dofork(void);
@@ -46,23 +51,56 @@ void _sig_handler(int);
 int main (int argc, char *argv[])
 {
         setlogmask (LOG_UPTO (SERD_LOG_LEVEL));
-        openlog (DAEMON_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+        openlog (DAEMON_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON);
 
         syslog(LOG_INFO, "Beginning init...");
 
+        /* Parse CLI Args: */
+        char c;
+
+        while (--argc > 0 && (*++argv)[0] == '-'){
+                c = *++argv[0];
+                switch (c) {
+                case 'p':
+                        if(argc-- >= 0) strcpy(pub_port, *++argv);
+                        syslog(LOG_DEBUG, "Argument -p has value %s", pub_port);
+                        break;
+                
+                case 's':
+                        if(argc-- >= 0) strcpy(use_tty, *++argv);
+                        syslog(LOG_DEBUG, "Argument -s has value %s", use_tty);
+                        break;
+                
+                default:
+                        syslog(LOG_INFO, "Called with undefined switch '%c'", c);
+                        break;
+                }
+        }
+        
+
+        if (argc != 0){
+                printf("Undefined arguments given; Usage: serd -p SOCKET_PORT_NUM -s SERIAL_PORT. Continuing.\n");
+                syslog(LOG_INFO, "Undefined arguments given; Usage: serd -p SOCKET_PORT_NUM -s SERIAL_PORT. Continuing.");
+        }
+        
+        
 	dofork();
 
 	/* Initialise serial: */
 	initserial();
 
+        char* tmp = malloc(strlen(SERD_SOCKET) + strlen(pub_port) +1);
+        sprintf(tmp, "%s%s", SERD_SOCKET, pub_port);
+
 	/* Initialise socket: */
-        pub = zsock_new_pub (">"SERD_SOCKET);
-	
+        pub = zsock_new_pub(tmp);
+        free(tmp);
+
         /* Handle kill signals */
 	signal(SIGINT, _sig_handler);
 	signal(SIGTERM, _sig_handler);
 
-	syslog(LOG_INFO, "Initialised successfully on %s using %s", SERD_SOCKET, DEFAULT_TTY);
+	syslog(LOG_INFO, "Initialised successfully on %s%s using %s", SERD_SOCKET, pub_port, use_tty);
 
 	while(1) rundaemon();
 }
@@ -102,14 +140,17 @@ void dofork(void)
         /* Set a new signature ID for the child process */
         sid = setsid();
 
-        if (sid < 0) exit(EXIT_FAILURE);
+        if (sid < 0) {
+                syslog(LOG_ERR, "Could not fork!");
+                killpg(pid,SIGINT);
+        }
 
         /*
          * Change DIR
          * If we can't change to the root directory then die
          */
         if ((chdir("/")) < 0) exit(EXIT_FAILURE);
-
+        
 	/* Close standard file descriptors */
         close(STDIN_FILENO);
         close(STDOUT_FILENO);
@@ -124,11 +165,11 @@ void initserial(void)
            O_NOCTTY    --  Do NOT use the TTY as this process' controlling terminal
            o_NONBLOCK  --  Do not allow operations on this file to cause blocking
         */
-	tty_file_desc = open(DEFAULT_TTY, O_RDWR | O_NOCTTY | O_NONBLOCK);
+	tty_file_desc = open(use_tty, O_RDWR | O_NOCTTY | O_NONBLOCK);
 
 	if (tty_file_desc < 0){
-                syslog(LOG_ERR, "Can't open serial port %s (errno %i)", DEFAULT_TTY, tty_file_desc);
-		exit(EXIT_FAILURE);
+                syslog(LOG_ERR, "Can't open serial port %s (errno %i)", use_tty, tty_file_desc);
+		killpg(getpid(),SIGINT);
 	}
 
 	/* Set file flags for serial port */
@@ -156,9 +197,10 @@ void _sig_handler(int signum)
 {
 	if(signum == SIGTERM || signum == SIGINT){
 		zsock_destroy(&pub);
-		syslog(LOG_NOTICE, "Exiting...");
+                syslog(LOG_INFO, "Exiting... (%i)", signum);
 		closelog();
                 close(tty_file_desc);
-		exit(EXIT_SUCCESS);
+                exit(EXIT_SUCCESS);
 	}
+        syslog(LOG_ERR, "Quit unexpectedly.");
 }
